7a0817bb25a86c89fe3cc14b861313d3320b0003
ismail-la <lahbariismail@gmail.com>
2023-03-16 19:17:42 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ printf("Size of char: %ld bytes\n", sizeof(char));
 printf("Size of short: %ld bytes\n", sizeof(short));
 printf("Size of int: %ld bytes\n", sizeof(int));
 printf("Size of long int: %ld bytes\n", sizeof(long int));
-printf("Size of long long int: %ld bytes\n", sizeof(long lon intg));
+printf("Size of long long int: %ld bytes\n", sizeof(long long int));
 printf("Size of float: %ld bytes\n", sizeof(float));
 return (0);
 }
