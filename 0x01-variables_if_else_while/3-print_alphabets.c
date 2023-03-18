4d0d1b23ce4b3873b2e70a797b31bc245cd10afe
ismail-la <lahbariismail@gmail.com>
2023-03-17 19:09:29 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Succecc)
+ */
+int main(void)
+{
+char alph[52]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
+for(i=0;i<52;i++)
+{
+putchar(alph[i]);
+}
+putchar('\n');
+return (0);
+}
