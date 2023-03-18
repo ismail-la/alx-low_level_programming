7a742ee9d790d4c5aa1943b94bc761d5cf42c715
ismail-la <lahbariismail@gmail.com>
2023-03-17 19:14:50 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,16 +3,17 @@
 /**
  * main - Prints the alphabet.
  *
- * Return: Always 0 (Succecc)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char alph[52]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-int i;
-for(i=0;i<52;i++)
-{
-putchar(alph[i]);
-}
-putchar('\n');
-return (0);
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
 }
