764e0aaf81a3a19a914f11320037ca887dc9ade3
ismail-la <lahbariismail@gmail.com>
2023-04-15 23:21:00 -0700

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,22 @@
 #include <stdio.h> 
- #include <stdlib.h> 
- #include "main.h" 
+#include <stdlib.h> 
+#include "main.h" 
   
  /** 
   * main - print product of argument numbers 
   * @argc: argument counter 
   * @argv: numbers to multiply 
-  * Return: 0 on success, 1 if two arguments not given 
+  * Return: 0 on success, 1 if two arguments are not given 
   */ 
   
  int main(int argc, char *argv[]) 
  { 
-         /* validate input */ 
-         if (argc != 3) 
-         { 
-                 printf("Error\n"); 
-                 return (1); 
-         } 
-  
-         /* multiply two arguments passed via cmd line */ 
-         printf("%d\n", atoi(argv[1]) * atoi(argv[2])); 
-         return (0); 
+ /* validate input */ 
+if (argc != 3) 
+ { 
+printf("Error\n"); 
+return (1); 
+ } 
+  /* multiply two arguments passed via cmd line */ printf("%d\n", atoi(argv[1]) * atoi(argv[2])); 
+return (0); 
  }
