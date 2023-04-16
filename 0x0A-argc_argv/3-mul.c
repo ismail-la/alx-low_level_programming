ccc40fcbc284602869c0cc60221f20e6cb1db82a
ismail-la <lahbariismail@gmail.com>
2023-04-15 23:48:06 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,45 +1,133 @@
-#include "main.h"                                                                                                                   
+#include <stdio.h>
 
-#include <stdlib.h>                                                                                                                 
+#include "main.h"
 
-#include <stdio.h>                                                                                                                   
 
-                                                                                                                                     
+/**
 
-/**                                                                                                                                 
+ * _atoi - converts a string to an integer
 
- * main - prints the multiplication of two integers                                                                                 
+ * @s: string to be converted
 
- * @argc: argument count                                                                                                             
+ *
 
- * @argv: argument vector                                                                                                           
+ * Return: the int converted from the string
 
- * Return: 0 if true, 1 if false                                                                                                     
+ */
 
- */                                                                                                                                 
+int _atoi(char *s)
 
-int main(int argc, char *argv[])                                                                                                    
+{
 
-{                                                                                                                                    
+        int i, d, n, len, f, digit;
 
-int a, b;                                                                                                                            
 
-if (argc == 3)                                                                                                                      
+        i = 0;
 
-{                                                                                                                                    
+        d = 0;
 
-a = atoi(argv[1]);                                                                                                                  
+        n = 0;
 
-b = atoi(argv[2]);                                                                                                                  
+        len = 0;
 
-printf("%d\n", a *b);                                                                                                                
+        f = 0;
 
-return (0);                                                                                                                          
+        digit = 0;
 
-}                                                                                                                                    
 
-printf("Error\n");                                                                                                                  
+        while (s[len] != 0)
 
-return (1);                                                                                                                          
+                len++;
+
+
+        while (i < len && f == 0)
+
+        {
+
+                if (s[i] == -)
+
+                        ++d;
+
+
+                if (s[i] >= 0 && s[i] <= 9)
+
+                {
+
+                        digit = s[i] - 0;
+
+                        if (d % 2)
+
+                                digit = -digit;
+
+                        n = n * 10 + digit;
+
+                        f = 1;
+
+                        if (s[i + 1] < 0 || s[i + 1] > 9)
+
+                                break;
+
+                        f = 0;
+
+                }
+
+                i++;
+
+        }
+
+
+        if (f == 0)
+
+                return (0);
+
+
+        return (n);
+
+}
+
+
+/**
+
+ * main - multiplies two numbers
+
+ * @argc: number of arguments
+
+ * @argv: array of arguments
+
+ *
+
+ * Return: 0 (Success), 1 (Error)
+
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+        int result, num1, num2;
+
+
+        if (argc < 3 || argc > 3)
+
+        {
+
+                printf("Error\n");
+
+                return (1);
+
+        }
+
+
+        num1 = _atoi(argv[1]);
+
+        num2 = _atoi(argv[2]);
+
+        result = num1 * num2;
+
+
+        printf("%d\n", result);
+
+
+        return (0);
 
 }
