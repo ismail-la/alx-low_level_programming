59a5cd17e8d35c86d619fdda48c1c9532dce8d1f
ismail-la <lahbariismail@gmail.com>
2023-04-15 23:43:40 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,277 +1,45 @@
-#include <stdio.h>
+#include "main.h"                                                                                                                   
 
-                
+#include <stdlib.h>                                                                                                                 
 
-#include "main.h"
+#include <stdio.h>                                                                                                                   
 
-                
+                                                                                                                                     
 
+/**                                                                                                                                 
 
-                
+ * main - prints the multiplication of two integers                                                                                 
 
-/**
+ * @argc: argument count                                                                                                             
 
-                
+ * @argv: argument vector                                                                                                           
 
- * _atoi - converts a string to an integer
+ * Return: 0 if true, 1 if false                                                                                                     
 
-                
+ */                                                                                                                                 
 
- * @s: string to be converted
+int main(int argc, char *argv[])                                                                                                    
 
-                
+{                                                                                                                                    
 
- *
+int a, b;                                                                                                                            
 
-                
+if (argc == 3)                                                                                                                      
 
- * Return: the int converted from the string
+{                                                                                                                                    
 
-                
+a = atoi(argv[1]);                                                                                                                  
 
- */
+b = atoi(argv[2]);                                                                                                                  
 
-                
+printf("%d\n", a *b);                                                                                                                
 
-int _atoi(char *s)
+return (0);                                                                                                                          
 
-                
+}                                                                                                                                    
 
-{
+printf("Error\n");                                                                                                                  
 
-                
-
-        int i, d, n, len, f, digit;
-
-                
-
-
-                
-
-        i = 0;
-
-                
-
-        d = 0;
-
-                
-
-        n = 0;
-
-                
-
-        len = 0;
-
-                
-
-        f = 0;
-
-                
-
-        digit = 0;
-
-                
-
-
-                
-
-        while (s[len] != 0)
-
-                
-
-                len++;
-
-                
-
-
-                
-
-        while (i < len && f == 0)
-
-                
-
-        {
-
-                
-
-                if (s[i] == -)
-
-                
-
-                        ++d;
-
-                
-
-
-                
-
-                if (s[i] >= 0 && s[i] <= 9)
-
-                
-
-                {
-
-                
-
-                        digit = s[i] - 0;
-
-                
-
-                        if (d % 2)
-
-                
-
-                                digit = -digit;
-
-                
-
-                        n = n * 10 + digit;
-
-                
-
-                        f = 1;
-
-                
-
-                        if (s[i + 1] < 0 || s[i + 1] > 9)
-
-                
-
-                                break;
-
-                
-
-                        f = 0;
-
-                
-
-                }
-
-                
-
-                i++;
-
-                
-
-        }
-
-                
-
-
-                
-
-        if (f == 0)
-
-                
-
-                return (0);
-
-                
-
-
-                
-
-        return (n);
-
-                
-
-}
-
-                
-
-
-                
-
-/**
-
-                
-
- * main - multiplies two numbers
-
-                
-
- * @argc: number of arguments
-
-                
-
- * @argv: array of arguments
-
-                
-
- *
-
-                
-
- * Return: 0 (Success), 1 (Error)
-
-                
-
- */
-
-                
-
-int main(int argc, char *argv[])
-
-                
-
-{
-
-                
-
-        int result, num1, num2;
-
-                
-
-
-                
-
-        if (argc < 3 || argc > 3)
-
-                
-
-        {
-
-                
-
-                printf("Error\n");
-
-                
-
-                return (1);
-
-                
-
-        }
-
-                
-
-
-                
-
-        num1 = _atoi(argv[1]);
-
-                
-
-        num2 = _atoi(argv[2]);
-
-                
-
-        result = num1 * num2;
-
-                
-
-
-                
-
-        printf("%d\n", result);
-
-                
-
-
-                
-
-        return (0);
-
-                
+return (1);                                                                                                                          
 
 }
