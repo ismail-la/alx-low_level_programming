e78905a46e6dfcb55fd472ddcfe49f418fccff64
ismail-la <lahbariismail@gmail.com>
2023-05-02 18:36:08 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,12 +9,12 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t numnodes = 0;
+	size_t numnode = 0;
 
 	while (h != NULL)
 	{
 		h = h->next;
-		numnodes++;
+		numnode++;
 	}
-	return (numnodes);
+	return (numnode);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -7,7 +7,7 @@
 
 /**
  * struct listint_s - singly linked list
- * @n: integer
+ * @n: an integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
@@ -34,4 +34,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
 #endif
