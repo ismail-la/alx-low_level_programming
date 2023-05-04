c4d9b43528032eff0bcdcdcab3cdfeba4f637620
ismail-la <lahbariismail@gmail.com>
2023-05-03 19:06:39 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,23 +4,23 @@
  * free_listp - frees linked list
  * @head: head of list
  *
- * Return: nop return
+ * Return: no return
  */
 void free_listp(listp_t **head)
 {
-	listp_t *temp;
-	listp_t *bir;
+        listp_t *temp;
+        listp_t *bir;
 
-	if (head != NULL)
-	{
-		bir = *head;
-		while ((temp = bir) != NULL)
-		{
-			bir = bir->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
+        if (head != NULL)
+        {
+                bir = *head;
+                while ((temp = bir) != NULL)
+                {
+                        bir = bir->next;
+                        free(temp);
+                }
+                *head = NULL;
+        }
 }
 
 
@@ -31,41 +31,40 @@ void free_listp(listp_t **head)
  * Return: number of the nodes in the list
  */
 
-
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t numnode = 0;
-	listp_t *hp, *new, *add;
+        size_t numnode = 0;
+        listp_t *hp, *new, *add;
 
-	hp = NULL;
-	while (head != NULL)
-	{
-		new = malloc(sizeof(listp_t));
+        hp = NULL;
+        while (head != NULL)
+        {
+                new = malloc(sizeof(listp_t));
 
-		if (new == NULL)
-		exit(98);
+                if (new == NULL)
+                        exit(98);
 
-		new->p = (void *)head;
-		new->next = hp;
-		hp = new;
+                new->p = (void *)head;
+                new->next = hp;
+                hp = new;
 
-		add = hp;
+                add = hp;
 
-while (add->next != NULL)
-{
-add = add->next;
-if (head == add->p)
-{
-printf("-> [%p] %d\n", (void *)head, head->n);
-free_listp(&hp);
-return (numnode);
-}
-}
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
-numnode++;
-	}
+                while (add->next != NULL)
+                {
+                        add = add->next;
+                        if (head == add->p)
+                        {
+                                printf("-> [%p] %d\n", (void *)head, head->n);
+                                free_listp(&hp);
+                                return (numnode);
+                        }
+                }
+                printf("[%p] %d\n", (void *)head, head->n);
+                head = head->next;
+                numnode++;
+        }
 
-free_listp(&hp);
-return (numnode);
+        free_listp(&hp);
+        return (numnode);
 }
