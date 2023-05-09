e4d176705ee18c6cf48d3577a91a381decbf9690
ismail-la <lahbariismail@gmail.com>
2023-05-08 18:21:08 -0700

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (N_len = 0; text_content[len];)
+		for (N_len = 0; text_content[N_len];)
 			N_len++;
 	}
 
