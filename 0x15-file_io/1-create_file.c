aace2a5e17f20f90fcd12cf2ead657bbbe460e7c
ismail-la <lahbariismail@gmail.com>
2023-05-08 18:23:38 -0700

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	/*file opened with permissions set to 0600which corresponds to rw-------.*/
-	wr = write(fd, text_content, len);
+	wr = write(fd, text_content, N_len);
 
 
 	if (fd == -1 || wr == -1)
