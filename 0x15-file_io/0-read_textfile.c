372aa4023256475080f278ca963c7906ce2f794a
ismail-la <lahbariismail@gmail.com>
2023-05-08 17:35:10 -0700

    0x15. C - File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile-  reads a text file and prints it to stdout
+ * @filename: text file or filename being read.
+ * @letters: number of letters to be read 
+ * Return: w- numbers of letters printed,
+ *        It fails, returns 0. or filename is NULL.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t fd, wr, rd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	rd = read(fd, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, rd);
+
+	free(buffer);
+	close(fd);
+	return (wr);
+}
