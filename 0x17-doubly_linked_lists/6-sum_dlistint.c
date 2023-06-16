1d340107dadb8c5335f41a866576d3e52f4e95aa
ismail-la <lahbariismail@gmail.com>
2023-06-15 18:42:34 -0700

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -9,12 +9,11 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
-	int sum = 0;
-	while (head)
-	{
-		sum += head->n;
-		head = head->next;
-	}
-
-	return (sum);
+int sum = 0;
+while (head)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
 }
