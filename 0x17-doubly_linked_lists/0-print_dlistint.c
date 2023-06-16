08e24708c48713ec1ba63ebd48ade63fa79499d1
ismail-la <lahbariismail@gmail.com>
2023-06-15 17:29:13 -0700

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,7 +10,14 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0;
+	count = 0;
+	int count;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
 	while (h)
 	{
 		printf("%d\n", h->n);
