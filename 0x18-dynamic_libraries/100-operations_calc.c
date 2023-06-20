7537c9ce5ea8929e6b90aaab14ecd7c895ed3298
ismail-la <lahbariismail@gmail.com>
2023-06-19 17:41:15 -0700

    0x18. C - Dynamic libraries

---
+++ 0x18-dynamic_libraries/100-operations_calc.c
@@ -0,0 +1,59 @@
+/**
+ * sub - Returns the subtracts of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: The difference of a - b.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * add - the sum of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: The sum of (a and b).
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * div - the division of two numbers.
+ * @a: first number
+ * @b: second number
+ *
+ * Return: The quotient of a / b.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mul - Multiply of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: The product of a and b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * mod - the remainder of the division of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: The remainder of the division of a by b.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
