c804b5e67e94c8ba1d63c9c4ba3bc0fb94c80d02
ismail-la <lahbariismail@gmail.com>
2023-10-11 18:32:07 -0700

    0x1E. C - Search Algorithms

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,5 +1,21 @@
 #include "search_algos.h"
+#include <math.h>
 
+/*Remember compiling "math.h" with gcc requires `-lm` */
+
+size_t min(size_t a, size_t b);
+
+/**
+ * min - returns the minimum of two size_t values
+ * @a: first value
+ * @b: second value
+ *
+ * Return: `a` if lower or equal to `b`, `b` otherwise
+ */
+size_t min(size_t a, size_t b)
+{
+	return (a <= b ? a : b);
+}
 
 /**
  * jump_search - Function that searches for a value in a sorted array of
